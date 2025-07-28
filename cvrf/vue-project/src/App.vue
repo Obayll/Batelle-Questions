7989329bb2c4f8bfa9e5d091d89a43c90cf1bbe8
Obayll <dajordster@gmail.com>
2025-07-27 20:23:51 -0400

    Editing vue app

--- cvrf/vue-project/src/App.vue
+++ cvrf/vue-project/src/App.vue
@@ -1,47 +1,30 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Input Echo App</h1>
+    <input v-model="userInput" type="text" placeholder="Type something..." />
+    <p>You typed: <strong>{{ userInput }}</strong></p>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const userInput = ref('')
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  max-width: 500px;
+  margin: 2rem auto;
+  padding: 1rem;
+  text-align: center;
+  font-family: Arial, sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+input {
+  padding: 0.5rem;
+  width: 100%;
+  font-size: 1rem;
+  margin-bottom: 1rem;
 }
 </style>
