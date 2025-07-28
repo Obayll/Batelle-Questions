d562684accf32d4788a482e6806f17706b0fad90
Obayll <dajordster@gmail.com>
2025-07-27 23:08:17 -0400

    Moved to shortened JSON file of actual values

--- cvrf/vue-project/src/App.vue
+++ cvrf/vue-project/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <h1>Fruit Search</h1>
+    <h1>Vulnerability Search</h1>
     <!-- store textbox information into query variable  -->
-    <input v-model="query" type="text" placeholder="Search for fuits:"/>
+    <input v-model="query" type="text" placeholder="Search for vulnerabilities..."/>
 
     <!-- call getResults() on button click  -->
     <button @click="getResults">Search</button>
 
     <!--
       if queryResults has any entries
-      loop through queryResults and display the name value
+      print matching index and vulnurability
       else print no items found
     -->
     <ul v-if="queryResults.length">
-      <li v-for="item in queryResults" :key="item.id">{{ item.name }}</li>
+      <div v-for="(vuln, index) in queryResults">
+        <h3>Match at index: {{ index }}</h3>
+        <pre>{{ vuln }}</pre>
+      </div>
     </ul>
     <p v-else>No results found.</p>
   </div>
@@ -31,22 +34,30 @@ const query = ref('')
 const queryResults = ref([])
 
 async function getResults() {
+  // Always reset queryResults to empty upon function enter
+  queryResults.value = []
+
   // Get user-input from textbox, removing extra whitespace, and convert to lower case
   const cleanQuery = query.value.trim().toLowerCase()
 
-  // If query contains nothing, set queryResults to nothing and return early
+  // If query contains nothing return early
   if (!cleanQuery) {
-    queryResults.value = []
     return
   }
 
   // Get response from json-server REST API
-  const response = await fetch('http://localhost:3000/items')
+  const response = await fetch('http://localhost:3000/cvrfdoc')
 
   // Convert query response to JSON format
   const responseJSON = await response.json()
 
-  // Iterate through JSON reponse and only keep entries with names containing a substring of the user-input
-  queryResults.value = responseJSON.filter(item => item.name.toLowerCase().includes(cleanQuery))
+  // Get vulnerabilities from top-level API call
+  // fetch('http://localhost:3000/cvrfdoc/Vulnerability') does not work since json-server only supports top-level API calls
+  const responseVuln = responseJSON.Vulnerability
+
+  // Takes an array of vulnerabilities and maps the data to an explicit index
+  // Then filters the data based on the underlying title to an exact match against the user-input
+  queryResults.value = responseVuln.map((vuln, index) => ({ index, data: vuln }))
+    .filter(({ data }) => data.Title.toLowerCase() == cleanQuery)
 }
 </script>
